a083eb1cd2331a208b543df3af8593159f6b8867
Andrej Kunizkij <anruxasmile7@gmail.com>
2019-03-26 20:11:51 +0300

    fix

--- main.js
+++ main.js
@@ -1,34 +1,39 @@
 
-setTimeout(function() {
-		document.body.style.display = 'block';
-	}, 5000);
-window.onload = function() {
-		document.body.style.display = 'none';
-	};
+/*
+setTimeout(function() { 
+document.body.style.display = 'block'; 
+}, 5000); 
+window.onload = function() { 
+document.body.style.display = 'none'; 
+}; 
+
+*/
 
 
-function close() {
-	 document.querySelector('.wrapper').style.display = "none";
+
+var arrText = ['item1','item2','item3','item4','item5','item6']; 
+var num = 0; 
+function next() { 
+var text = document.getElementById('changeText'); 
+num++ 
+if(num >= arrText.length) { 
+num = 0; 
+} 
+text.innerHTML = arrText[num]; 
+} 
+
+function prev() { 
+var text = document.getElementById('changeText'); 
+num--; 
+if(num<0) { 
+num = arrText.length-1; 
+} 
+text.innerHTML = arrText[num]; 
+} 
+
+
+function func() { 
+// alert('Страница была загружена!'); 
+var elem = document.getElementById('hop'); 
+elem.style.display = 'none'; 
 }
-
-
-
-var arrText = ['item1','item2','item3','item4','item5','item6'];
-var num = 0;
-	function next() {
-		var	text = document.getElementById('changeText');
-		 num++
-		if(num>=arrText.length) {
-		 num=0;
-		}
-		 text.innerHTML = arrText[num];
-		}
-
-function prev() {
-	 var text = document.getElementById('changeText');
-	 num--;
-	 if(num<0) {
-	 	num = arrText.length-1;
-	 }
-	  text.innerHTML = arrText[num];
-}		
